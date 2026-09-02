17fd98331402f95cee820b99bddeded7800373e5
Ayush <ayushhsingh2002@gmail.com>
2026-09-03 00:00:20 +0530

    Binary Exponentiation - Optimal

---
+++ Recursion-Part-1/BinaryExponentiation.java
@@ -0,0 +1,18 @@
+class Solution {
+    public double myPow(double x, int n) {
+        // Optimal solution
+        long N = n;
+        if(N<0) {
+            x = 1/x;
+            N = -N;
+        }
+        double answer = 1;
+        while(N>0) {
+            if(N%2==1) answer *= x;
+            x *= x;
+            N /=2;
+        }
+        return answer;
+    }
+}
+// Topics -> Recursion, Math, LC-50
