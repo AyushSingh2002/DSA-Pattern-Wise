0c8b19a99d3d3f74da9606819277ce28f87831d9
Ayush <ayushhsingh2002@gmail.com>
2026-09-19 02:09:28 +0530

    Missing Number

---
+++ Bit-Manipulation/MissingNumber.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int xor1 = 0;
+        int xor2 = 0;
+        for(int i=1; i<=n; i++) xor1 = xor1^i;
+        for(int i=0; i<n; i++) xor2 = xor2^nums[i];
+        return xor1^xor2;
+    }
+}
+// Topics -> Bit Manipulation, TC: O(n), SC: O(1), LC-268
