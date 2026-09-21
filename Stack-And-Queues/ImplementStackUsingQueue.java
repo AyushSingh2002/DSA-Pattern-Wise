e77682a21c820a93092c3550a591597bf7bad0f9
Ayush <ayushhsingh2002@gmail.com>
2026-09-21 23:17:32 +0530

    Implement Queue Using Stack, Implement Stack Using Queue

---
+++ Stack-And-Queues/ImplementStackUsingQueue.java
@@ -0,0 +1,40 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+
+    private Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        int size = q.size();
+        q.add(x);
+        for(int i=0; i<size; i++) q.add(q.poll());
+    }
+    
+    public int pop() {
+        return q.poll();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
+
+// Topics -> Stack, TC: O(n), SC: O(n), LC-225
