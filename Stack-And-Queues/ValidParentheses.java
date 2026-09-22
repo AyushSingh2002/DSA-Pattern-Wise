45fd82e282acbceb93164e4ee25109ce350f5d42
Ayush <ayushhsingh2002@gmail.com>
2026-09-22 23:17:10 +0530

    Valid Parentheses

---
+++ Stack-And-Queues/ValidParentheses.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '[') stack.push(ch);
+            else {
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if((ch == ')' && top == '(') || (ch == '}' && top == '{') || (ch == ']' && top == '[')){}
+                else return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+// Topics -> Stack, TC: O(n), SC: O(n), LC-20
